7ab5bdd13f611f4e2687ed34f45052af8897ce2e
Alin <ai6@tuta.io>
2023-02-08 10:44:03 +0000

    cleanup code

--- Assets/Scripts/Astar/EnemyMovement.cs
+++ Assets/Scripts/Astar/EnemyMovement.cs
@@ -6,13 +6,14 @@ public class EnemyMovement : MonoBehaviour
 {
     public FloatVariableSO speed;
     public IntVariableSO reachDistance;
+    [SerializeField]
     private int currentWaypoint = 0;
     private List<Node> path = new List<Node>();
+    [SerializeField]
+    private int pathCount = 0;
     public Transform target;
     private Vector3 lastTargetPos;
-    private Vector3 lastEnemyPos;
     private Astar aStar;
-
     public Animator animator;
     public SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
@@ -20,66 +21,55 @@ public class EnemyMovement : MonoBehaviour
     void Start()
     {
         lastTargetPos = target.position;
-        lastEnemyPos = transform.position;
         aStar = GetComponent<Astar>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
-    void Update()
+    void FixedUpdate()
     {
         if(target == null)
-        {
-            animator.SetBool("isMoving", false);
             return;
-        }
-        if(target.position != lastTargetPos ){
+
+        if(target.position != lastTargetPos)
+        {
+            currentWaypoint = 0;
             lastTargetPos = target.position;
-            lastEnemyPos = transform.position;
             findPath(transform.position, target.position);
         }
         
+        animator.SetBool("isMoving", false);
+        rb.velocity = Vector2.zero;
+
         if (path.Count > 0)
         {
-            
+            // For debugging
+            pathCount = path.Count;
+
             if (currentWaypoint <= path.Count - reachDistance.Value)
             {
                 animator.SetBool("isMoving", true);
+
                 Vector3 dir = (path[currentWaypoint].worldPosition - transform.position);
                 dir = Vector3.ClampMagnitude(dir.normalized, 0.5f); 
+
                 if (rb.velocity.normalized != (Vector2)dir)
-                {
                     rb.velocity = new Vector2(dir.x * speed.Value, dir.y * speed.Value);
-                }
+
                 if (dir.x > 0)
-                {
                     spriteRenderer.flipX = true;
-                }
                 else
-                {
                     spriteRenderer.flipX = false;
-                }
 
                 if (Vector3.Distance(transform.position, path[currentWaypoint].worldPosition) < 0.1f) 
-                {
                     currentWaypoint++;
-                }
-            }
-            else
-            {
-                animator.SetBool("isMoving", false);
-                rb.velocity = Vector2.zero;
             }
             if(currentWaypoint > path.Count)
             {
                 animator.SetBool("isMoving", false);
+                rb.velocity = Vector2.zero;
                 currentWaypoint = 0;
             }
-
-        }
-        else
-        {
-            animator.SetBool("isMoving", false);
         }
     }
 
