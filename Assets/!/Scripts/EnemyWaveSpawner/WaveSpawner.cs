b686677e9b9dcce5ce1aae2bcae06beea7361ae3
DanielImparatelu <danielimparatelu@gmail.com>
2023-05-26 17:56:25 +0100

    Add zombie spawn mechanics

---
+++ Assets/!/Scripts/EnemyWaveSpawner/WaveSpawner.cs
@@ -0,0 +1,83 @@
+using Assets.Scripts.Stats;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public List<Enemy> enemies = new List<Enemy>();
+    public int currWave;
+    public int waveValue;
+    public List<GameObject> enemiesToSpawn = new List<GameObject>();
+
+    public Transform spawnLocation;
+    public int waveDuration;
+    private float waveTimer;
+    private float spawnInterval;
+    private float spawnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerrateWave();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (spawnTimer <= 0)
+        {
+            if (enemiesToSpawn.Count > 0)
+            {
+                Instantiate(enemiesToSpawn[0], spawnLocation.position, Quaternion.identity);
+                enemiesToSpawn.RemoveAt(0);
+                spawnTimer = spawnInterval;
+            }
+            else
+            {
+                waveTimer = 0;
+            }
+        }
+        else
+        {
+            spawnTimer -= Time.fixedDeltaTime;
+            waveTimer -= Time.fixedDeltaTime;
+        }
+    }
+
+    public void GenerrateWave()
+    {
+        waveValue = currWave * 10;
+        GenerateEnemies();
+
+        spawnInterval = waveDuration / enemiesToSpawn.Count;
+        waveTimer = waveDuration;
+    }
+    public void GenerateEnemies()
+    {
+        List<GameObject> generateEnemies = new List<GameObject>();
+        while (waveValue > 0)
+        {
+            int randEnemyId = Random.Range(0, enemies.Count);
+            int randEnemyCost = enemies[randEnemyId].cost;
+
+            if (waveValue - randEnemyCost >= 0)
+            {
+                generateEnemies.Add(enemies[randEnemyId].enemyPrefab);
+                waveValue -= randEnemyCost;
+            }
+            else if (waveValue <= 0)
+            {
+                break;
+            }
+        }
+        enemiesToSpawn.Clear();
+        enemiesToSpawn = generateEnemies;
+    }
+}
+
+[System.Serializable]
+public class Enemy
+{
+    public GameObject enemyPrefab;
+    public int cost;
+}
