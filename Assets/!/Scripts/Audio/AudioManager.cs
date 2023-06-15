1b6abf40568ba5896c3b0e63ff4b8059e00c53ae
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2023-06-15 21:44:15 +0100

    added source delay config

--- Assets/!/Scripts/Audio/AudioManager.cs
+++ Assets/!/Scripts/Audio/AudioManager.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
   public int AudioSourcesLimit;
-  public AudioEvent[] audioEvents;
+  public AudioEvent[] AudioEvents;
+  [MinMaxRange(0.1f, 5f)]
+  public RangedFloat SourceDelay;
 
   private AudioSource[] AudioSources;
   // Start is called before the first frame update
@@ -25,14 +27,14 @@ public class AudioManager : MonoBehaviour
   {
     while (true)
     {
-      var randomAudioEventSelector = new RandomOptionSelector(audioEvents.Length);
+      var randomAudioEventSelector = new RandomOptionSelector(AudioEvents.Length);
       foreach (var audioSource in AudioSources)
       {
         //select clip type. I.E. Zombie1, Zombie2
         var selectedIndexClipType = randomAudioEventSelector.GetRandomOption();
-        AudioEvent selectedAudioEvent = audioEvents[selectedIndexClipType];
+        AudioEvent selectedAudioEvent = AudioEvents[selectedIndexClipType];
         //select random clip from random clip type
-        float sourceDelay = UnityEngine.Random.Range(0.5f, 1.5f);
+        float sourceDelay = UnityEngine.Random.Range(SourceDelay.minValue, SourceDelay.maxValue);
         yield return new WaitForSeconds(sourceDelay);
         selectedAudioEvent.Play(audioSource);
       }
